0ac9793f84ed96b9c65aacb3cb1d23151d311962
paprikaLang <3060866223@qq.com>
2019-01-02 17:13:22 +0800

    yield Request(url=next_page_url, callback=self.parse)

--- BoleSpider/BoleSpider/spiders/jobbole.py
+++ BoleSpider/BoleSpider/spiders/jobbole.py
@@ -1,32 +1,26 @@
 # -*- coding: utf-8 -*-
 import scrapy
 import re
+from scrapy.http import Request
+from urllib import parse
 
 class JobboleSpider(scrapy.Spider):
     name = 'jobbole'
     allowed_domains = ['blog.jobbole.com']
-    start_urls = ['http://blog.jobbole.com/110287/']
+    start_urls = ['http://blog.jobbole.com/all-posts/']
 
     def parse(self, response):
-        title = response.xpath('//*[@id="post-110287"]/div[1]/h1/text()').extract_first()
-        create_date = response.xpath("//p[@class='entry-meta-hide-on-mobile']/text()").extract_first().strip().replace("·", "").strip()
-        votes =response.xpath("//span[contains(@class, 'vote-post-up')]/h10/text()").extract_first()
 
-        favs = response.xpath("//span[contains(@class, 'bookmark-btn')]/text()").extract_first()
-        match_re = re.match(".*?(\d+).*", favs)
-        if match_re:
-            favs = match_re.group(1)
+        page_posts_urls = response.css("#archive .floated-thumb .post-thumb a::attr(href)").extract()
 
-        comments = response.xpath("//a[@href='#article-comment']/span/text()").extract_first()
-        match_re = re.match(".*?(\d+).*", comments)
-        if match_re:
-            comments = match_re.group(1)
+        for post_url in page_posts_urls:
+            yield Request(url=parse.urljoin(response.url, post_url), callback=self.parse_post)
 
-        body = response.xpath("//div[@class='entry']").extract_first()
-        tags = response.xpath("//p[@class='entry-meta-hide-on-mobile']/a/text()").extract()
-        tags = [el for el in tags if not el.strip().endswith("评论")]
-        tags = ",".join(tags)
+        next_page_url = response.css(".next.page-numbers::attr(href)").extract_first("")
+        if next_page_url:
+            yield Request(url=parse.urljoin(response.url, next_page_url), callback=self.parse)
 
+    def parse_post(self, response):
         # css选择器
         title = response.css(".entry-header h1::text").extract_first().strip()
         create_date = response.css(".entry-meta-hide-on-mobile::text").extract_first().strip().replace("·", "").strip()
@@ -34,24 +28,45 @@ class JobboleSpider(scrapy.Spider):
         bookmarks = response.css(".bookmark-btn::text").extract_first()
         match_re = re.match(".*?(\d+).*", bookmarks)
         if match_re:
-            bookmarks = match_re.group(1)
+            bookmarks = int(match_re.group(1))
+        else:
+            bookmarks = 0
 
         comments = response.css("a[href='#article-comment'] span::text").extract_first()
         match_re = re.match(".*?(\d+).*", comments)
         if match_re:
-            comments = match_re.group(1)
+            comments = int(match_re.group(1))
+        else:
+            comments = 0
 
         body = response.css("div.entry").extract_first()
 
         tags = response.css("p.entry-meta-hide-on-mobile a::text").extract()
         tags = [el for el in tags if not el.strip().endswith("评论")]
         tags = ",".join(tags)
+
+
         pass
 
 
-
-
-
+        # title = response.xpath('//*[@id="post-110287"]/div[1]/h1/text()').extract_first()
+        # create_date = response.xpath("//p[@class='entry-meta-hide-on-mobile']/text()").extract_first().strip().replace("·", "").strip()
+        # votes =response.xpath("//span[contains(@class, 'vote-post-up')]/h10/text()").extract_first()
+        #
+        # favs = response.xpath("//span[contains(@class, 'bookmark-btn')]/text()").extract_first()
+        # match_re = re.match(".*?(\d+).*", favs)
+        # if match_re:
+        #     favs = match_re.group(1)
+        #
+        # comments = response.xpath("//a[@href='#article-comment']/span/text()").extract_first()
+        # match_re = re.match(".*?(\d+).*", comments)
+        # if match_re:
+        #     comments = match_re.group(1)
+        #
+        # body = response.xpath("//div[@class='entry']").extract_first()
+        # tags = response.xpath("//p[@class='entry-meta-hide-on-mobile']/a/text()").extract()
+        # tags = [el for el in tags if not el.strip().endswith("评论")]
+        # tags = ",".join(tags)
 
 
 
