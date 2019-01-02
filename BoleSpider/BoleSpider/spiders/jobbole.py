53f3f9784e7f28771c8c40ecf6a0e3c33437049e
paprikaLang <3060866223@qq.com>
2019-01-02 16:08:01 +0800

    scrapy shell url &  response.css

--- BoleSpider/BoleSpider/spiders/jobbole.py
+++ BoleSpider/BoleSpider/spiders/jobbole.py
@@ -10,9 +10,9 @@ class JobboleSpider(scrapy.Spider):
     def parse(self, response):
         title = response.xpath('//*[@id="post-110287"]/div[1]/h1/text()').extract_first()
         create_date = response.xpath("//p[@class='entry-meta-hide-on-mobile']/text()").extract_first().strip().replace("·", "").strip()
-        praises =response.xpath("//span[contains(@class, 'vote-post-up')]/h10/text()").extract_first()
-        # extract_first() 为空时有默认值 不会像 extract()[0] 抛错
-        favs = response.xpath("//span[contains(@class, 'bookmark-btn')]/text()").extract()[0]
+        votes =response.xpath("//span[contains(@class, 'vote-post-up')]/h10/text()").extract_first()
+
+        favs = response.xpath("//span[contains(@class, 'bookmark-btn')]/text()").extract_first()
         match_re = re.match(".*?(\d+).*", favs)
         if match_re:
             favs = match_re.group(1)
@@ -24,6 +24,26 @@ class JobboleSpider(scrapy.Spider):
 
         body = response.xpath("//div[@class='entry']").extract_first()
         tags = response.xpath("//p[@class='entry-meta-hide-on-mobile']/a/text()").extract()
+        tags = [el for el in tags if not el.strip().endswith("评论")]
+        tags = ",".join(tags)
+
+        # css选择器
+        title = response.css(".entry-header h1::text").extract_first().strip()
+        create_date = response.css(".entry-meta-hide-on-mobile::text").extract_first().strip().replace("·", "").strip()
+        votes = response.css(".vote-post-up h10::text").extract_first()
+        bookmarks = response.css(".bookmark-btn::text").extract_first()
+        match_re = re.match(".*?(\d+).*", bookmarks)
+        if match_re:
+            bookmarks = match_re.group(1)
+
+        comments = response.css("a[href='#article-comment'] span::text").extract_first()
+        match_re = re.match(".*?(\d+).*", comments)
+        if match_re:
+            comments = match_re.group(1)
+
+        body = response.css("div.entry").extract_first()
+
+        tags = response.css("p.entry-meta-hide-on-mobile a::text").extract()
         tags = [el for el in tags if not el.strip().endswith("评论")]
         tags = ",".join(tags)
         pass
