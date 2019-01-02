8a8de3a01faa40613677e69204300b5204072853
paprikaLang <3060866223@qq.com>
2019-01-02 19:48:51 +0800

    yield items -> pipelines -> images

--- BoleSpider/BoleSpider/items.py
+++ BoleSpider/BoleSpider/items.py
@@ -12,3 +12,16 @@ class BolespiderItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
     pass
+
+class BolePostItem(scrapy.Item):
+    title = scrapy.Field()
+    create_date = scrapy.Field()
+    url = scrapy.Field()
+    url_object_id = scrapy.Field()
+    preview_img = scrapy.Field()
+    preview_img_path = scrapy.Field()
+    votes = scrapy.Field()
+    comments = scrapy.Field()
+    bookmarks = scrapy.Field()
+    tags = scrapy.Field()
+    body = scrapy.Field()
--- BoleSpider/BoleSpider/spiders/jobbole.py
+++ BoleSpider/BoleSpider/spiders/jobbole.py
@@ -3,6 +3,7 @@ import scrapy
 import re
 from scrapy.http import Request
 from urllib import parse
+from BoleSpider.items import BolePostItem
 
 class JobboleSpider(scrapy.Spider):
     name = 'jobbole'
@@ -11,16 +12,20 @@ class JobboleSpider(scrapy.Spider):
 
     def parse(self, response):
 
-        page_posts_urls = response.css("#archive .floated-thumb .post-thumb a::attr(href)").extract()
+        page_posts_nodes = response.css("#archive .floated-thumb .post-thumb a")
 
-        for post_url in page_posts_urls:
-            yield Request(url=parse.urljoin(response.url, post_url), callback=self.parse_post)
+        for post_node in page_posts_nodes:
+            post_url = post_node.css("::attr(href)").extract_first("")
+            image_url = post_node.css("img::attr(src)").extract_first("")
+            yield Request(url=parse.urljoin(response.url, post_url), meta={"preview_img": image_url}, callback=self.parse_post)
 
         next_page_url = response.css(".next.page-numbers::attr(href)").extract_first("")
         if next_page_url:
             yield Request(url=parse.urljoin(response.url, next_page_url), callback=self.parse)
 
     def parse_post(self, response):
+        post_item = BolePostItem()
+
         # css选择器
         title = response.css(".entry-header h1::text").extract_first().strip()
         create_date = response.css(".entry-meta-hide-on-mobile::text").extract_first().strip().replace("·", "").strip()
@@ -44,9 +49,19 @@ class JobboleSpider(scrapy.Spider):
         tags = response.css("p.entry-meta-hide-on-mobile a::text").extract()
         tags = [el for el in tags if not el.strip().endswith("评论")]
         tags = ",".join(tags)
+        preview_img = response.meta.get("preview_img", "")
 
+        post_item["title"] = title
+        post_item["url"] = response.url
+        post_item["create_date"] = create_date
+        post_item["preview_img"] = [preview_img]
+        post_item["votes"] = votes
+        post_item["comments"] = comments
+        post_item["bookmarks"] = bookmarks
+        post_item["body"] = body
+        post_item["tags"] = tags
 
-        pass
+        yield post_item
 
 
         # title = response.xpath('//*[@id="post-110287"]/div[1]/h1/text()').extract_first()
